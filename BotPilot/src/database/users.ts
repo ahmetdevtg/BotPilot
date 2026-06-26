4e07ad9229f84ad118ac75a59bf75453f3c3357d
ahmetdevtg <yrdahmets@gmail.com>
2026-06-26 19:27:19 +0300

    Update users.ts

--- BotPilot/src/database/users.ts
+++ BotPilot/src/database/users.ts
@@ -11,3 +11,68 @@ export async function getUserByUsername(
     .first();
 
 }
+export async function getUsers(
+  db: D1Database
+) {
+  const { results } = await db
+    .prepare(`
+      SELECT *
+      FROM users
+      ORDER BY id ASC
+    `)
+    .all();
+
+  return results;
+}
+
+export async function createUser(
+  db: D1Database,
+  username: string,
+  passwordHash: string
+) {
+  return await db
+    .prepare(`
+      INSERT INTO users
+      (
+        username,
+        password_hash
+      )
+      VALUES(?,?)
+    `)
+    .bind(
+      username,
+      passwordHash
+    )
+    .run();
+}
+
+export async function updatePassword(
+  db: D1Database,
+  id: number,
+  passwordHash: string
+) {
+  return await db
+    .prepare(`
+      UPDATE users
+      SET password_hash=?
+      WHERE id=?
+    `)
+    .bind(
+      passwordHash,
+      id
+    )
+    .run();
+}
+
+export async function deleteUser(
+  db: D1Database,
+  id: number
+) {
+  return await db
+    .prepare(`
+      DELETE FROM users
+      WHERE id=?
+    `)
+    .bind(id)
+    .run();
+}
