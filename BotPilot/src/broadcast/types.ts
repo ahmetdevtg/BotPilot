2c7a07b5dd36b2ff4e4e4cadd82282f6bc9fb17a
ahmetdevtg <yrdahmets@gmail.com>
2026-06-26 14:07:00 +0300

    Update types.ts

--- BotPilot/src/broadcast/types.ts
+++ BotPilot/src/broadcast/types.ts
@@ -1,1 +1,12 @@
-.
+export interface BroadcastButton {
+  text: string;
+  url: string;
+}
+
+export interface BroadcastMessage {
+  text: string;
+  photo?: string;
+  video?: string;
+  document?: string;
+  buttons?: BroadcastButton[];
+}
