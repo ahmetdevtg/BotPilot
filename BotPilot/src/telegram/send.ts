0cd224b776006dede35d71ca2a67304472a465d7
ahmetdevtg <yrdahmets@gmail.com>
2026-06-26 16:50:35 +0300

    Update send.ts

--- BotPilot/src/telegram/send.ts
+++ BotPilot/src/telegram/send.ts
@@ -19,3 +19,39 @@ export async function sendMessage(
 
   return await res.json();
 }
+export async function sendPhotoWithButton(
+  token: string,
+  chatId: number,
+  photo: string,
+  caption: string,
+  buttonText: string,
+  buttonUrl: string
+) {
+
+  const res = await fetch(
+    `https://api.telegram.org/bot${token}/sendPhoto`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        chat_id: chatId,
+        photo,
+        caption,
+        parse_mode: "HTML",
+        reply_markup: {
+          inline_keyboard: [[
+            {
+              text: buttonText,
+              url: buttonUrl
+            }
+          ]]
+        }
+      })
+    }
+  );
+
+  return await res.json();
+
+}
