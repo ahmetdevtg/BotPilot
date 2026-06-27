252cc69c982edc54690deed66b11aa508a6544d6
ahmetdevtg <yrdahmets@gmail.com>
2026-06-27 22:14:17 +0300

    Update bot.service.ts

--- BotPilot/src/services/bot.service.ts
+++ BotPilot/src/services/bot.service.ts
@@ -3,8 +3,10 @@ import {
   getBotByTelegramId
 } from "../database/bots";
 
-import { getMe } from "../telegram/api";
-import { setWebhook } from "../telegram/api";
+import {
+  getMe,
+  setWebhook
+} from "../telegram/api";
 
 export async function addBot(
   db: D1Database,
@@ -33,10 +35,70 @@ export async function addBot(
     token,
     me.result.id
   );
+
+  // Global ayarı oku
+  const globalSettings: any = await db
+    .prepare(`
+      SELECT *
+      FROM bot_settings
+      ORDER BY id ASC
+      LIMIT 1
+    `)
+    .first();
+  if (globalSettings) {
+
+    await db
+      .prepare(`
+        INSERT INTO bot_settings
+        (
+          bot_id,
+          start_message,
+          photo,
+          video,
+          document_url,
+          button_text,
+          button_url,
+          parse_mode,
+          is_enabled
+        )
+        VALUES
+        (
+          ?,?,?,?,?,?,?,?,?
+        )
+      `)
+      .bind(
+        me.result.id,
+        globalSettings.start_message,
+        globalSettings.photo,
+        globalSettings.video,
+        globalSettings.document_url,
+        globalSettings.button_text,
+        globalSettings.button_url,
+        globalSettings.parse_mode,
+        globalSettings.is_enabled
+      )
+      .run();
+
+  } else {
+
+    await db
+      .prepare(`
+        INSERT INTO bot_settings
+        (
+          bot_id
+        )
+        VALUES(?)
+      `)
+      .bind(
+        me.result.id
+      )
+      .run();
+
+  }
   await setWebhook(
     token,
     "https://botpilot.yrdahmets.workers.dev/webhook/" + me.result.id
-);
+  );
 
   return me.result;
 
