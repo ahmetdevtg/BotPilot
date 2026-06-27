e992d70bcb4566553d9ed55d9c46e96a2128649a
ahmetdevtg <yrdahmets@gmail.com>
2026-06-28 00:29:59 +0300

    Update send.ts

--- BotPilot/src/broadcast/send.ts
+++ BotPilot/src/broadcast/send.ts
@@ -89,7 +89,7 @@ export async function sendBroadcast(
           options.buttonUrl,
           options.parseMode || "HTML"
         );
-      else if (
+      } else if (
         options.buttonText &&
         options.buttonUrl
       ) {
